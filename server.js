9d96106111004f364dbf2fbe8d4a286a6d43d046
0xkairem <106383479+0xkairem@users.noreply.github.com>
2022-11-30 06:07:34 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 require("dotenv").config();
 const express = require("express");
+const http = require("node:http");
 const cors = require("cors");
 const app = express();
+
+const host = "127.0.0.1";
 const port = process.env.PORT || 80;
 
 app.use(express.json());
 
-const corsOptions = {
-  origin: "http://localhost:3005",
-  optionsSuccessStatus: 200,
-  methods: "GET",
+// http://192.168.1.67:3000
+
+var whitelist = [
+  "http://127.0.0.1:3000",
+  "http://localhost:3000",
+  "http://127.0.0.1",
+  "http://192.168.1.67:3000",
+  "https://merry-bienenstitch-7db92b.netlify.app",
+];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
-app.use(cors(corsOptions));
+app.get("/message", cors(corsOptions), (req, res) => {
+  res.json({ message: `${process.env.PRIVATE_KEY}` });
+});
 
-// test route
-app.get("/", (req, res) => res.json({ message: "Test Message" }));
+// // test route
+// app.get("/test", (req, res) => res.json({ message: "Test Message" }));
 
-app.use("/message", require("./routes/message"));
+// app.use("/message", require("./routes/message"));
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(port, host, () =>
+  console.log(`App listening on at http://${host}:${port}`)
+);
